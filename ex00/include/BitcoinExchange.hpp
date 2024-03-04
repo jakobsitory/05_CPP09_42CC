3496f07962b6c48f68592cd3b7169ef651eba438
Jakob Schott <jakob.schott@gmail.com>
2024-03-04 12:45:06 +0100

    some minor preparations for tomorrow

--- ex00/include/BitcoinExchange.hpp
+++ ex00/include/BitcoinExchange.hpp
@@ -6,17 +6,31 @@
 /*   By: jschott <jschott@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/01 14:54:34 by jschott           #+#    #+#             */
-/*   Updated: 2024/03/04 10:22:58 by jschott          ###   ########.fr       */
+/*   Updated: 2024/03/04 12:14:10 by jschott          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#ifndef BITCOINEXCHANCE_HPP
+# define BITCOINEXCHANCE_HPP
+
+#include <string>
+#include <iostream>
+#include <iomanip>
 
 class BitcoinExchange
 {
 private:
-	int
+	
 public:
 	BitcoinExchange();
 	BitcoinExchange(BitcoinExchange & origin);
 	BitcoinExchange & operator=(BitcoinExchange & origin);
 	~BitcoinExchange();
+
+	bool	validDate(std::string &datestring);
+	bool	validVal(std::string &valstring);
+	void	parseInput(std::string filename);
+	
 };
+
+#endif
--- ex00/src/BitcoinExchange.cpp
+++ ex00/src/BitcoinExchange.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   BitcoinExchange.cpp                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jschott <jschott@student.42.fr>            +#+  +:+       +#+        */
+/*   By: jschott <jschott@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/01 14:54:42 by jschott           #+#    #+#             */
-/*   Updated: 2024/03/01 14:59:44 by jschott          ###   ########.fr       */
+/*   Updated: 2024/03/04 12:35:21 by jschott          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,4 +28,60 @@ BitcoinExchange & BitcoinExchange::operator=(BitcoinExchange & origin){
 
 BitcoinExchange::~BitcoinExchange(){
 	
+}
+
+bool	validDate(std::string &datestring){
+	if (datestring.length() != 10){
+		std::cerr << COLOR_ERROR << "Error: Bad input => " << datestring << COLOR_STANDARD << std::endl;
+		return false;
+	}
+		
+			
+}
+
+bool	validVal(std::string &valstring){
+	float	val;
+	char*	error;
+
+	val = strtof(valstring.c_str(), &error);
+	if (error != "f"){
+		std::cerr << "Error: bad value." << std::endl;
+		return false;
+	}
+	else if ( val < 0){
+		std::cerr << "Error: not a positive number." << std::endl;
+		return false;
+	}
+	else if (val > 1000){
+		std::cerr << "Error: too large a number." << std::endl;
+		return false;
+	}
+	return true;	
+}
+
+void	parseInput(std::string filename){
+	
+}
+
+std::vector<std::string> Btc::splitString(std::string str, char delimiter)
+{
+	std::vector<std::string> substrings;
+	std::string substring = "";
+	for (size_t i = 0; i < str.length(); i++)
+	{
+		if (str[i] != delimiter)
+		{
+			substring += str[i];
+		}
+		else
+		{
+			substrings.push_back(substring);
+			while (str[i] == delimiter)
+				i++;
+			i--;
+			substring = "";
+		}
+	}
+	substrings.push_back(substring);
+	return substrings;
 }
