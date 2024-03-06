75b719fbb1a83687d8dca340c268c991f00ed9bb
Jakob Schott <jakob.schott@gmail.com>
2024-03-06 17:07:14 +0100

    working ex01 + formatting

--- ex01/src/main.cpp
+++ ex01/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jschott <jschott@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/01 14:54:53 by jschott           #+#    #+#             */
-/*   Updated: 2024/03/06 16:58:19 by jschott          ###   ########.fr       */
+/*   Updated: 2024/03/06 17:06:48 by jschott          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,14 +19,14 @@ int main (int argc, char **argv){
 		return 1;
 	}
 	if (strlen(argv[1]) == 1 && isdigit(argv[1][0]))
-		std::cout << (argv[1]) << std::endl;
+		std::cout << TEXT_BOLD <<  (argv[1]) << TEXT_NOFORMAT <<  std::endl;
 	
 	else {
 		try {
 			RPN rpn;
 			rpn.validInput(argv[1]);
 			int result = rpn.sequenceRPN(argv[1]);
-			std::cout << "Result: " TEXT_BOLD << result << TEXT_BOLD << std::endl;
+			std::cout << TEXT_BOLD << result << TEXT_BOLD << std::endl;
 		}
 		catch(const std::exception& e)
 		{
